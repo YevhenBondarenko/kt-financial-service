b813ab75839744e0479b6c772ecdd1c25b0ef434
YevhenBondarenko <bondarenkojek@gmail.com>
2025-10-05 17:55:20 +0200

    implemented Financial service

--- src/main/kotlin/mate/academy/FinancialService.kt
+++ src/main/kotlin/mate/academy/FinancialService.kt
@@ -7,8 +7,9 @@ class FinancialService {
         amount: CurrencyAmount,
         currencyCode: CurrencyCode,
         transactionId: TransactionId
-    ) : String {
-        // TODO: implement
+    ): String {
+        return "Transferred ${amount.amount} ${currencyCode.code} from ${source.number} to ${destination.number}. " +
+                "Transaction ID: ${transactionId.id}"
     }
 
     fun convertCurrency(
@@ -16,7 +17,8 @@ class FinancialService {
         fromCurrency: CurrencyCode,
         toCurrency: CurrencyCode
     ): CurrencyAmount {
-        // TODO: implement
+        val rate = getExchangeRate(fromCurrency, toCurrency)
+        return CurrencyAmount(amount.amount * rate)
     }
 
     private fun getExchangeRate(fromCurrency: CurrencyCode, toCurrency: CurrencyCode): Double {
@@ -26,5 +28,37 @@ class FinancialService {
             fromCurrency.code == "USD" && toCurrency.code == "GBP" -> 0.82
             else -> 1.0
         }
+    }
+}
+
+private const val CODE_LENGTH = 3
+private const val ZERO = 0
+private const val ACCOUNT_LENGTH = 10
+
+@JvmInline
+value class CurrencyCode(val code: String) {
+    init {
+        require(code.length == CODE_LENGTH && code.all { it.isUpperCase() }) { "Wrong currency code format: $code" }
+    }
+}
+
+@JvmInline
+value class CurrencyAmount(val amount: Double) {
+    init {
+        require(amount >= ZERO) { "Amount cannot be negative: $amount" }
+    }
+}
+
+@JvmInline
+value class TransactionId(val id: String) {
+    init {
+        require(id.isNotEmpty()) { "Wrong transaction id format: $id" }
+    }
+}
+
+@JvmInline
+value class AccountNumber(val number: String) {
+    init {
+        require(number.length == ACCOUNT_LENGTH && number.all { it.isDigit() }) { "Wrong account number format: $number" }
     }
 }
